b53c87898b905681be62569a69e31bccb03d15e4
Deependra Neupane <Deependra Neupane>
2017-07-10 01:30:41 +0545

    added email

--- power/forms.py
+++ power/forms.py
@@ -2,5 +2,8 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(required=True)
+    name.widget.attrs.update({'class': 'form-control'})
     email = forms.EmailField(required=True)
+    email.widget.attrs.update({'class': 'form-control'})
     question = forms.CharField(widget=forms.Textarea)
+    question.widget.attrs.update({'class': 'form-control'})
