573df13872e4320d55bf18166c4766002499c8fe
deependra neupane <deependran.neupane@gmail.com>
2017-08-06 18:23:51 +0545

    hello

--- OpenPowerEngineering/settings.py
+++ OpenPowerEngineering/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'mwszsb=_8e*y=(pft)-)2jw)chm%=k9^wip)lqd(p1rs8r^de_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kathmandu'
 
 USE_I18N = True
 
